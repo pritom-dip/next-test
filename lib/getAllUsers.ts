ab3534da7b779e1d04d0deec7fde8b760f9e7c7b
pritom <pritomdip010@gmail.com>
2023-10-06 22:56:34 +0100

    Server side data fetch

---
+++ lib/getAllUsers.ts
@@ -0,0 +1,7 @@
+const getAllUsers = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users")
+    if(!res.ok) throw new Error ('failed to fetch data')
+    return res.json()
+}
+
+export default getAllUsers
